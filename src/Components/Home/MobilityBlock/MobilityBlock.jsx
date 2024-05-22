180ebd6a39ffd82ff08a43d670005f0c7ea7fdc3
Omar Terrazas <terrazas.omar@gmail.com>
2024-05-21 20:47:51 -0600

    Ajustes de texto en MobilityBlock

--- src/Components/Home/MobilityBlock/MobilityBlock.jsx
+++ src/Components/Home/MobilityBlock/MobilityBlock.jsx
@@ -8,7 +8,7 @@ function MobilityBlock() {
           <div className="mobility-block__central--block">
             <h2>Movilidad estudiantil</h2>
             <h3>Navega la diversidad, conquista tu futuro</h3>
-            <p>La UNAM abre sus puertas al mundo con su programa de movilidad estudiantil, ofreciendo una experiencia única de intercambio cultural y académico. Sumérgete en nuevas culturas, conecta con estudiantes internacionales y amplía tu conocimiento en prestigiosas universidades. Descubre, aprende y vive tu carrera desde una perspectiva global. ¡Únete y transforma tu futuro con la movilidad estudiantil de la UNAM!</p>
+            <p>La UNAM abre sus puertas al mundo con su programa de movilidad estudiantil, ofreciendo una experiencia única de intercambio cultural y académico. Sumérgete en nuevas culturas, conecta con estudiantes internacionales y amplía tu conocimiento en prestigiosas universidades. Descubre, aprende y vive tu carrera desde una perspectiva global. ¡Únete y transforma tu futuro con la movilidad estudiantil de la UNAM!</p>
             <p className="mobility-block__central--block-link">
               <NavLink to="movilidad">Más información</NavLink>
             </p>
