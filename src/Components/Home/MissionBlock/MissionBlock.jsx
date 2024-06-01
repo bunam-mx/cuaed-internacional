8c30a48cb51f18d40b83677aca13eaec61c0868b
Omar Terrazas <terrazas.omar@gmail.com>
2024-05-31 19:43:04 -0600

    Ajuste en disposición de elementos en MissionBlock

--- src/Components/Home/MissionBlock/MissionBlock.jsx
+++ src/Components/Home/MissionBlock/MissionBlock.jsx
@@ -4,13 +4,6 @@ function MissionBlock() {
   return (
     <section className="mission-block">
       <div className="mission-block__content">
-        <div className="mission-block__content--image">
-          <img
-            src="/images/mission-block.jpg"
-            alt="La misión de CUAED Internacional"
-            width={480}
-          />
-        </div>
         <div className="mission-block__content--text">
           <h2>Misión</h2>
           <p>Promover la internacionalización en el SUAyED a través de:</p>
@@ -26,6 +19,13 @@ function MissionBlock() {
             para que más estudiantes, docentes y funcionarios SUAyED aprendan
             mientras potencian su sensibilidad y competencias multiculturales.
           </p>
+        </div>
+        <div className="mission-block__content--image">
+          <img
+            src="/images/mission-block.jpg"
+            alt="La misión de CUAED Internacional"
+            width={480}
+          />
         </div>
       </div>
     </section>
