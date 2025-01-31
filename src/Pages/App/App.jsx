0ca5cf6efb3b1f43d1d32b6f4517747e377fbaf1
Omar Terrazas <terrazas.omar@gmail.com>
2025-01-30 18:10:42 -0600

    Inicialización del template

---
+++ src/Pages/App/App.jsx
@@ -0,0 +1,23 @@
+import { useRoutes, BrowserRouter } from "react-router-dom";
+import HeaderBar from "../../Components/HeaderBlock/HeaderBlock";
+// import FooterBlock from "../../Components/FooterBlock/FooterBlock";
+import Home from "../Home/Home";
+import './App.css'
+
+const AppRoutes = () => {
+  return useRoutes([
+    { path: "/", element: <Home /> }
+  ]);
+};
+
+function App() {
+  return (
+    <BrowserRouter>
+      <HeaderBar />
+      <AppRoutes />
+      { /* <FooterBlock /> */ }
+    </BrowserRouter>
+  )
+}
+
+export default App
